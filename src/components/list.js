ec427498eebe5582995193283815234f96f29e9c
ForAnyMove <alex.osadchuk.98@gmail.com>
2022-03-21 19:25:00 +0200

    second

--- src/components/list.js
+++ src/components/list.js
@@ -3,15 +3,13 @@ import Task from "./task";
 import '../styles/list.css';
 
 const loop = () => {
-    ()=>{
-        for(let i = 0; i< 4; i++){
-            return <Task text={i}></Task>
-        }  
-    }
+    console.group(document.getElementById('todo-list'))
+    return <Task text={'gdgffdg'}></Task>
     
 }
 
 function TaskList(props) {
+
   return (
       <ul id="todo-list" className="todo-list">
           <Task text="test task 1"></Task>
